9697cd7f62abb6cf67f5a1d25cfdb303d802ee86
Pedro Bellarmino Cardoso <126488720+PedroBellarminoCardoso@users.noreply.github.com>
2025-11-20 15:12:15 -0300

    Delete script directory

--- script/ddl/Tabelas/Instrutores.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Instrutores (
-    instrutor_id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-    nome VARCHAR(80) NOT NULL,
-    cpf CHAR(11) UNIQUE,
-    telefone VARCHAR(20),
-    registro_CREF VARCHAR(10) DEFAULT 'Pleno',
-    ativo BIT NOT NULL DEFAULT 1,
-    contratado_em DATE
-);
--- script/ddl/Tabelas/Modalidades.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Modalidades (
-    modalidade_id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-    nome VARCHAR(60) NOT NULL,
-    descricao VARCHAR(200),
-    instrutor_principal_id INT NULL,
-    ativo BIT NOT NULL DEFAULT 1,
-    FOREIGN KEY (instrutor_principal_id)
-        REFERENCES Instrutores(instrutor_id)
-);
