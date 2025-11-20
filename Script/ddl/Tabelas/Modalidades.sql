5ccd24242c4d7588e38bf3fe21c71b2a3d3ef930
Pedro Bellarmino Cardoso <126488720+PedroBellarminoCardoso@users.noreply.github.com>
2025-11-20 15:18:25 -0300

    Create Modalidades.sql

---
+++ Script/ddl/Tabelas/Modalidades.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Modalidades (
+    modalidade_id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    nome VARCHAR(40) NOT NULL,
+    descricao VARCHAR(200),
+    instrutor_principal_id INT NULL,
+    ativo BIT NOT NULL DEFAULT 1,
+    FOREIGN KEY (instrutor_principal_id)
+        REFERENCES Instrutores(instrutor_id)
+);
